90a948e3bbb1087e908e94f0de1def6d1fc1c726
Omphile91 <158012917+Omphile91@users.noreply.github.com>
2024-06-05 16:30:47 +0200

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=8
+b=2
+
+print( 'Addition:\t\t' , a , '+' , b , '=' , a + b )
+print( 'Subtraction:\t\t' , a , '-' , b , '=' , a - b )
+print( 'Multiplication:\t/t' , a , 'x' , b , '=' , a * b )
+
+print( 'Division:\t' , a , '÷' , b , '=' , a / b )
+print( 'Floor Division:\t\t' , a , '÷' , b , '=' , a // b )
+
+print( 'Modulo:\t\t\t' , a , '%' , b , '=' , a % b )
+
+print( 'Exponent:\t\t ' , a , '² = ' , a ** b , sep = '' )
